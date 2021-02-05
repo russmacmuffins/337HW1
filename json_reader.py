691063059ec60cfd82814853fcd821e2120f351f
tani-s <61853265+tani-s@users.noreply.github.com>
2021-02-05 13:32:13 -0600

    filtering stopwords

--- json_reader.py
+++ json_reader.py
@@ -1,5 +1,11 @@
 import json
 import nltk
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+
+OFFICIAL_AWARDS_1315 = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
+OFFICIAL_AWARDS_1819 = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
 
 def get_tweets(pathname):
@@ -32,13 +38,13 @@ def get_contains(tweets, word):
 def get_most_common(tweets, num, award):
     award = nltk.word_tokenize(award)
     d = {}
-    banned = ['best', 'won', 'GoldenGlobes','Golden','Globes','Globe','goldenglobes']
+    banned = ['best', 'wins', 'GoldenGlobes','Golden','Globes','Globe','goldenglobes']
     for a in award:
         banned.append(a)
     for i in range(len(tweets)):
         t = nltk.word_tokenize(tweets[i])
-        for w in t:
-            if len(w) > 3 and w not in banned:
+        for w in t:  # changed to 1 because some names (i.e., "Ben" or "Li") are short, and we can use stopwords here
+            if len(w) > 1 and w not in stop_words and w not in banned:
                 if w in d:
                     d[w] = d[w] + 1
                 else:
@@ -54,10 +60,13 @@ def get_most_common(tweets, num, award):
     return mx_key
 
 
-
-gwon = get_contains(tweets,"won")
-gbest = get_contains(gwon,"best")
+gwins = get_contains(tweets,"wins")
+gbest = get_contains(gwins,"best")
 gdirector = get_contains(gbest,"director")
+print(get_most_common(gdirector, 0, OFFICIAL_AWARDS_1315[11]))
 
+def intersect(lst1, lst2):  # using sets would be faster, but we can't because they remove duplicates
+    lst3 = [t for t in lst1 if t in lst2]
+    return lst3
 
 1+1
